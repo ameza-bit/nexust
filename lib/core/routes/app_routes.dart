36a88185d278ccdb65ef9c25d291037f7f07ded3
Armando Mini <ameza@bit.lat>
2025-04-25 23:39:11 -0600

    Agregar configuración de navegación y pantalla de pestañas, incluyendo el tema neutral y la barra de navegación

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:nexust/ui/screens/auth/login_screen.dart';
+import 'package:nexust/ui/screens/home/tabs_screen.dart';
 
 class AppRoutes {
   static RouterConfig<Object>? getGoRoutes(
@@ -9,8 +10,19 @@ class AppRoutes {
     List<RouteBase> routes = [
       GoRoute(
         path: "/",
-        builder: (context, state) => const LoginScreen(),
-        routes: [],
+        builder: (context, state) => const TabsScreen(),
+        routes: [
+          GoRoute(
+            name: LoginScreen.routeName,
+            path: LoginScreen.routeName,
+            builder: (context, state) => const LoginScreen(),
+          ),
+          GoRoute(
+            name: TabsScreen.routeName,
+            path: TabsScreen.routeName,
+            builder: (context, state) => const TabsScreen(),
+          ),
+        ],
       ),
     ];
 
