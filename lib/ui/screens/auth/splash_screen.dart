4d52aec0cf886dc324afcbc2c7d59ef1dc6c3248
Armando MacBP <ameza@bit.lat>
2025-04-30 16:27:28 -0600

    feat: Convertir SplashScreen en StatefulWidget y añadir temporizador para navegación

--- lib/ui/screens/auth/splash_screen.dart
+++ lib/ui/screens/auth/splash_screen.dart
@@ -1,13 +1,38 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nexust/ui/screens/home/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => context.goNamed(HomeScreen.routeName),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset('assets/images/icon.png', width: 200, height: 200),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/icon.png', width: 200, height: 200),
+            const SizedBox(height: 24),
+            const CircularProgressIndicator(),
+          ],
+        ),
       ),
     );
   }
