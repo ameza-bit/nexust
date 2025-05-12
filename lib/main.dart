026239a62bcecc637e00fa4555104d112737b5b2
Armando MacBP <ameza@bit.lat>
2025-05-12 15:34:57 -0600

    feat: Añadir soporte para navegación y temas, incluyendo pantalla de inicio y configuración de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:nexust/core/routes/app_routes.dart';
+import 'package:nexust/core/themes/main_theme.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
 
-  runApp(const MainApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('es')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('es'),
+      child: const MainApp(),
+    ),
+  );
 }
 
+// ignore: prefer-match-file-name
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    final routerConfig = AppRoutes.getGoRoutes(navigatorKey);
+
+    return MaterialApp.router(
+      title: 'Nexust',
+      routerConfig: routerConfig,
+      theme: MainTheme.lightTheme.copyWith(
+        // primaryColor: state.settings.primaryColor,
+        textTheme: MainTheme.lightTheme.textTheme.apply(
+          // fontSizeFactor: state.settings.fontSize,
+        ),
+      ),
+      darkTheme: MainTheme.darkTheme.copyWith(
+        // primaryColor: state.settings.primaryColor,
+        textTheme: MainTheme.darkTheme.textTheme.apply(
+          // fontSizeFactor: state.settings.fontSize,
+        ),
+      ),
+      // themeMode: state.settings.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      // locale: Locale(state.settings.language),
     );
   }
 }
