f389e79b9a4de94308d9b2d4db008f8266a91344
Armando MacBP <ameza@bit.lat>
2025-05-12 15:04:58 -0600

    feat: Añadir configuración de Firebase y soporte para localización en la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
   runApp(const MainApp());
 }
 
@@ -10,11 +14,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
