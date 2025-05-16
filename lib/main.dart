b3adaffe8a3796a3b43f6306eaa1b27b89870aaf
Armando MacBP <ameza@bit.lat>
2025-05-15 22:16:11 -0600

    feat: Añadir soporte para autenticación de Firebase en múltiples plataformas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nexust/core/enums/language.dart';
@@ -17,7 +18,7 @@ Future<void> main() async {
   await EasyLocalization.ensureInitialized();
 
   await Firebase.initializeApp(
-    name: 'Nexust',
+    name: kIsWeb ? null : 'Nexust',
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
